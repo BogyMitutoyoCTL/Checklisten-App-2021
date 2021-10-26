42bdfeecb6f7cf678a9188834d7a4aa1225687ba
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2021-10-26 07:38:26 +0200

    Leere Checklisten-App

---
+++ Source/checklist_app/README.md
@@ -0,0 +1,3 @@
+# checklist_app
+
+BOGY Checklisten-App
---
+++ Source/checklist_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BogyChecklistApp());
+}
+
+class BogyChecklistApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
